e74718aa6f87dee91c51c1c5ce53d24d6594b3fd
Francois Rigaut <francois.rigaut@anu.edu.au>
2022-06-09 22:14:28 +0200

    added setup dual monitor

---
+++ hyprland_setup_dual_monitors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hyprctl dispatch moveworkspacetomonitor "1 1"
+hyprctl dispatch moveworkspacetomonitor "2 1"
+hyprctl dispatch moveworkspacetomonitor "4 1"
+hyprctl dispatch moveworkspacetomonitor "5 1"
+hyprctl dispatch moveworkspacetomonitor "6 1"
+hyprctl dispatch moveworkspacetomonitor "3 0"
+hyprctl dispatch moveworkspacetomonitor "7 0"
+hyprctl dispatch moveworkspacetomonitor "8 0"
+hyprctl dispatch moveworkspacetomonitor "9 0"
+hyprctl dispatch moveworkspacetomonitor "10 0"
